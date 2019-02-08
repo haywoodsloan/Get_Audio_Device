d938373dbf33607f1d8bc5de82d31f4f02d408d5
Sloan Haywood <haywood.sloan@yahoo.com>
2019-02-08 15:08:13 -0800

    Initial commit

---
+++ Get_Audio_Device/main.cpp
@@ -0,0 +1,49 @@
+#include <mmdeviceapi.h>
+#include <functiondiscoverykeys_devpkey.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+wstring getActiveAudioDeviceName(ERole role)
+{
+	IMMDeviceEnumerator* enumerator;
+	CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
+		CLSCTX_ALL, __uuidof(IMMDeviceEnumerator),
+		(void**)&enumerator);
+
+	IMMDevice* device;
+	enumerator->GetDefaultAudioEndpoint(eRender, role, &device);
+
+	IPropertyStore* props;
+	device->OpenPropertyStore(STGM_READ, &props);
+
+	PROPVARIANT nameProp;
+	PropVariantInit(&nameProp);
+	props->GetValue(PKEY_Device_FriendlyName, &nameProp);
+	wstring name = wstring(nameProp.pwszVal);
+
+	props->Release();
+	device->Release();
+	enumerator->Release();
+	PropVariantClear(&nameProp);
+
+	return name;
+}
+
+int main(int argc, char** argv)
+{
+	ERole role = eMultimedia;
+	if (argc > 0)
+	{
+		int value = atoi(argv[0]);
+		if (value == 2)
+		{
+			role = eCommunications;
+		}
+	}
+
+	CoInitialize(NULL);
+	wcout << getActiveAudioDeviceName(role) << endl;
+}
